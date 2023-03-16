962f18ffbddbe1dbb3badecbeca0a4a73d4f23d1
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2023-03-16 20:52:03 +0100

    cssChanges

---
+++ homePage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Księgarnia "Książek"</title>
+    <style type="text/css">
+ 	@import url('CSS/navBar.css');
+</style>
+
+</head>
+<body>
+            <nav class="navbar">
+                <ul>
+          <li><input type="text" placeholder="Wyszukaj..."></li>
+                    <li><a href="homePage.php">Strona główna</a></li>
+                    <li><a href="#">Koszyk</a></li>
+                    <li>
+                    <a href="HTML/login.php">Logowanie</a>
+                </li>
+                </ul>
+            </nav>
+        </div>
+
+
+
+<head> 
+    
+<style type="text/css">
+ 	@import url('CSS/menu.css');
+</style>
+</head>
+        <div class = "menu">
+            <ul>
+        <a>Kategorie</a>
+            <li><a href="#">Fantastyka</a></li>
+                    <li><a href="#">Kryminał</a></li>
+                    <li><a href="#">Reportaż</a></li>
+</ul>
+        </div>
+</body>
+</html>
