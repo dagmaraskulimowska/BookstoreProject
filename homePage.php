5e5e4d06548dee86f87079f1acda36f25c1e4198
Dagmara Skulimowska <dagmaraskul@wp.pl>
2023-04-03 12:28:06 +0200

    new-categories

--- homePage.php
+++ homePage.php
@@ -35,16 +35,20 @@ if ($database -> connect_errno) {
    <h1>Kategorie</h1>
    <nav class="menu">
       <div class="menu-column">
-         <a href="#" class="menu-item is-active">Dla dzieci</a>
+         <a href="HTML/ForKids.php" class="menu-item is-active">Dla dzieci</a>
          <a href="HTML/FantasyList.php" class="menu-item">Fantastyka</a>
-         <a href="#" class="menu-item">Thriller/Kryminał</a>
-         <a href="#" class="menu-item">Horror</a>
+         <a href="HTML/ThrillerCriminal.php" class="menu-item">Thriller/Kryminał</a>
+         <a href="HTML/Horror.php" class="menu-item">Horror</a>
+         <a href="HTML/CommonLiterature.php" class="menu-item">Literatura obyczajowa</a>
+         <a href="HTML/Business.php" class="menu-item">Biznes</a>
       </div>
       <div class="menu-column">
-         <a href="#" class="menu-item">Literatura piękna</a>
-         <a href="#" class="menu-item">Literatura obyczajowa</a>
-         <a href="#" class="menu-item">Literatura faktu</a>
-         <a href="#" class="menu-item">Rozwój osobisty</a>
+         <a href="HTML/BellesLettres.php" class="menu-item">Literatura piękna</a>
+         <a href="HTML/NonFiction.php" class="menu-item">Literatura faktu</a>
+         <a href="HTML/PersonalDevelopment.php" class="menu-item">Rozwój osobisty</a>
+         <a href="HTML/Romance.php" class="menu-item">Romans</a>
+         <a href="HTML/Historical.php" class="menu-item">Historyczne</a>
+         <a href="HTML/Scientific.php" class="menu-item">Naukowe</a>
       </div>
    </nav>
 </div>
