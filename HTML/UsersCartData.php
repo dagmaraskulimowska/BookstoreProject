119c86bfa2fc75c48fc9ad977bc1eda75284923f
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2023-04-12 23:11:58 +0200

    cart-update2

--- HTML/UsersCartData.php
+++ HTML/UsersCartData.php
@@ -7,10 +7,11 @@ session_start();
     <meta charset="UTF-8">
     <title>Księgarnia "Książek"</title>
     <link rel="stylesheet" type="text/css" href="../CSS/navigationBar.css">
-    <link rel="stylesheet" type="text/css" href="../CSS/loginPage.css">
     <link rel="stylesheet" type="text/css" href="../CSS/sideBar.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/usersCartData.css">
 </head>
 <body>
+
             <nav class="navbar">
                 <ul>
           <li><input type="text" placeholder="Wyszukaj..."></li>
@@ -21,10 +22,19 @@ session_start();
                 </li>
                 </ul>
             </nav>
-        </div>  
+        </div> 
+        <div class="center">
+  <a href="../html/cart.php" class="back-link">Powrót do koszyka</a>
+</div>
+<div id="return-link-container">
+        <a href="../HTML/cart.php">Powrót do koszyka</a>
+                </div>
+<div id="payment-link-container">
+        <a href="#">Przejdź do płatności</a>
+                </div>
+</body> 
 
         <?php
-
 try {
     $connection = new mysqli("localhost", "root", "", "logowanie");
 
@@ -41,6 +51,35 @@ try {
               header('Location: login.php');
               exit();
             }
+
+             // WYŚWIETLANIE DANYCH W TEXT BOXACH
+             if (mysqli_num_rows($reply) > 0) {
+              $row = mysqli_fetch_assoc($reply);
+              echo '<div class="center">';
+              echo '<form>';
+              echo '<label for="Imie">Imię:</label>';
+              echo '<div class="txt_field">';
+              echo '<input type="text" id="Imie" name="Imie" value="' . $row['Imie'] . '"><br></div>'; // tu dodajemy zamknięcie div
+              echo '<label for="Nazwisko">Nazwisko:</label>';
+              echo '<div class="txt_field">';
+              echo '<input type="text" id="Nazwisko" name="Nazwisko" value="' . $row['Nazwisko'] . '"><br></div>'; // tu dodajemy zamknięcie div
+              echo '<label for="Adres">Adres:</label>';
+              echo '<div class="txt_field">';
+              echo '<input type="text" id="Adres" name="Adres" value="' . $row['Adres'] . '"><br></div>'; // tu dodajemy zamknięcie div
+              echo '<label for="Kodpocztowy">Kod pocztowy:</label>';
+              echo '<div class="txt_field">';
+              echo '<input type="text" id="Kodpocztowy" name="Kodpocztowy" value="' . $row['Kodpocztowy'] . '"><br></div>'; // tu dodajemy zamknięcie div
+              echo '<label for="Miasto">Miasto:</label>';
+              echo '<div class="txt_field">';
+              echo '<input type="text" id="Miasto" name="Miasto" value="' . $row['Miasto'] . '"><br></div>'; // tu dodajemy zamknięcie div
+              echo '<label for="Nrtel">Numer telefonu:</label>';
+              echo '<div class="txt_field">';
+              echo '<input type="text" id="Nrtel" name="Nrtel" value="' . $row['Nrtel'] . '"><br></div>'; // tu dodajemy zamknięcie div
+              echo '<label for="Adresemail">Adres email:</label>';
+              echo '<div class="txt_field">';
+              echo '<input type="text" id="Adresemail" name="Adresemail" value="' . $row['Adresemail'] . '"><br></div>'; // tu dodajemy zamknięcie div
+              echo '</form>';
+            }
             $connection->close();
             exit();
     }
@@ -49,21 +88,5 @@ try {
     echo 'Error occured!';
 }
 ?>
-
-    <head>
-        <style type="text/css">
-  @import url('../CSS/login.css');
-    </style>
-    </head>
-        <input type="submit" value="Zapłać">
-
-      </form>
-      <?php
-    if(isset($_SESSION['error'])){ //jezeli istnieje zmienna error to ją wypisuje // <div class="pass">Zapomniałeś hasła?</div> 
-      echo '<span style="color: red; font-weight: bold;">'.$_SESSION['error'].'</span>'; 
-      unset($_SESSION['error']);
-    }
-  ?>
     </div>
-</body>
 </html>
