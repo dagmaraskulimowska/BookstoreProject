7422caf9b76c4f9e40d5b357cf2a9f4e1decc854
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2023-04-13 19:37:34 +0200

    mailWorking

--- HTML/sendEmail.php
+++ HTML/sendEmail.php
@@ -6,26 +6,52 @@ require '../vendor/phpmailer/phpmailer/src/Exception.php';
 require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
 require '../vendor/phpmailer/phpmailer/src/SMTP.php';
 
+session_start();
 
-    $name = "Ksiegarnia Ksiazek";
-    $email = "ksiegarniaksiazek@gmail.com";
-    $subject = "Dagmara i Tomek";
-    $message = file_get_contents("mailTemplate.php");
+// Sprawdzenie czy koszyk istnieje w sesji
+if (!isset($_SESSION['koszyk'])) {
+    $_SESSION['koszyk'] = array();
+}
 
-    $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'ksiegarniaksiazek@gmail.com';
-    $mail->Password = 'cygikokgsxcgqzzd';
-    $mail->Port = 465;
-    $mail->SMTPSecure = 'ssl';
-    $mail->isHTML(true);
-    $mail->setFrom($email, $name);
-    $mail->addAddress('dagaskul@wp.pl');
-    $mail->Subject = ("$email ($subject)");
-    $mail->Body = $message;
-    $mail->send();
+// Pobranie zawartości koszyka i wyliczenie kosztu zamówienia
+$koszyk = $_SESSION['koszyk'];
+$suma = 0;
+$zawartoscKoszyka = '';
+foreach ($koszyk as $produkt) {
+    $suma += $produkt['cena'] * $produkt['ilosc'];
+    $zawartoscKoszyka .= "{$produkt['tytul']} ({$produkt['ilosc']} szt.) - {$produkt['cena']} zł<br>";
+}
 
-    header("Location: ./response.php");
+// Pobranie adresu email zalogowanego użytkownika
+$connection = new mysqli("localhost", "root", "", "logowanie");
+$login = $_SESSION['login'];
+$query = "SELECT Adresemail FROM users WHERE login = '$login'";
+$result = $connection->query($query);
+$row = $result->fetch_assoc();
+$email = $row['Adresemail'];
+
+// Ustawienia mailera
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'ksiegarniaksiazek@gmail.com';
+$mail->Password = 'cygikokgsxcgqzzd';
+$mail->Port = 465;
+$mail->SMTPSecure = 'ssl';
+$mail->isHTML(true);
+$mail->setFrom('ksiegarniaksiazek@gmail.com', 'Ksiegarnia Ksiazek');
+$mail->addAddress($email);
+$mail->Subject = "Potwierdzenie zamowienia w Ksiegarni Ksiazek";
+$mail->Body = "Dziękujemy za zamówienie w Księgarni Książek!<br><br>Zawartość zamówienia:<br>{$zawartoscKoszyka}<br>Łączna kwota zamówienia: {$suma} zł";
+$mail->send();
+
+// Zamknięcie połączenia z bazą danych
+$connection->close();
+
+// Usunięcie zawartości koszyka po wysłaniu zamówienia
+$_SESSION['koszyk'] = array();
+
+// Przekierowanie do strony potwierdzenia zamówienia
+header("Location: ./response.php");
 ?>
