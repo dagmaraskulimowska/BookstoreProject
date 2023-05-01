32ab9d3f6f007a45fc64780216921b50ffe0fcd4
Dagmara Skulimowska <dagmaraskul@wp.pl>
2023-05-01 13:43:11 +0200

    updated-logs

--- HTML/logger.php
+++ HTML/logger.php
@@ -1,5 +1,5 @@
 <?php
-  $log_file = 'C:/xampp/htdocs/Workspace/BookstoreProject/log.txt'; // nazwa pliku dziennika
+  $log_file = '../BookstoreProject/log.txt'; // nazwa pliku dziennika
   $ip_address = $_SERVER['REMOTE_ADDR']; // adres IP użytkownika
   $page_name = $_SERVER['REQUEST_URI']; // nazwa aktualnie przeglądanej strony
   $time_stamp = date('Y-m-d H:i:s'); // czas zdarzenia nawigacyjnego
